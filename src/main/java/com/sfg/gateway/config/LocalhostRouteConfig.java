a44b04c26d407b50d11b12e29085dfbae5aa9438
griesba <griesba.pat@hotmail.com>
2020-07-22 23:05:03 +0200

    256 Beer Service Gateway route configuration

---
+++ src/main/java/com/sfg/gateway/config/LocalhostRouteConfig.java
@@ -0,0 +1,19 @@
+package com.sfg.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalhostRouteConfig {
+
+    @Bean
+    RouteLocator localhostRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/vi/beerUpc/*")//any path matching "/api/v1/beer/*" and "/api/vi/beerUpc/*"
+                        .uri("http://localhost:8080")
+                        .id("beer-service"))
+                .build();
+    }
+}
